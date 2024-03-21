7e9d820c1fe8e5616541d61d30c5932ae3aad866
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2024-03-21 15:35:16 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -15,7 +15,7 @@ const req = (arr, i) => {
 };
 
 request(
-  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  `https://swapi-api.alx-tools.com/films/${process.argv[2]}`,
   (err, response, body) => {
     if (err) {
       throw err;
