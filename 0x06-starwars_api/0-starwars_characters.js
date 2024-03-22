36813447c4048663c46e0149a289f23da6470b27
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2024-03-22 22:13:56 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,39 +1,38 @@
 #!/usr/bin/node
-
-// Import the 'request' library
+/**
+ * A script that prints all the characters of a Star Wars Movie.
+ */
 const request = require('request');
+const movieId = process.argv[2];
 
-// Define constant with the base URL of the Star Wars API
-const API_URL = 'https://swapi-api.alx-tools.com/api';
-
-// Check if the number of command line arguments is greater than 2
-if (process.argv.length > 2) {
-  // Make a request to the film resource for the specified film ID
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    // If an error occurred during the request, log the error
-    if (err) {
-      console.log(err);
-    }
-    // Get the characters URL from the film's response body
-    const charactersURL = JSON.parse(body).characters;
-
-    // Create an array of Promises that resolve with the names of the characters
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        // Make a request to the character resource
-        request(url, (promiseErr, __, charactersReqBody) => {
-          // If an error occurred during the request, reject the Promise with the error
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          // Resolve the Promise with the name of the character
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    // Wait for all Promises to resolve and log the names of the characters, separated by new lines
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+if (!movieId) {
+  console.log('Please provide a movie ID');
+  process.exit(1);
 }
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(url, async (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    try {
+      const fetchCharacterName = (characterUrl) => {
+        return new Promise((resolve, reject) => {
+          request.get(characterUrl, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+              resolve(JSON.parse(body).name);
+            } else {
+              reject(new Error('Character information request failed!'));
+            }
+          });
+        });
+      };
+      for (const characterUrl of JSON.parse(body).characters) {
+        console.log(await fetchCharacterName(characterUrl));
+      }
+    } catch (err) {
+      console.log('Exception:', err);
+    }
+  } else {
+    console.log('Movie information request failed!');
+  }
+});
