c8d6782fa7420af43e44a5d65c440ddca8a81ec2
ksdahmani10 <121550471+ksdahmani10@users.noreply.github.com>
2024-03-23 05:05:51 +0000

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,38 +1,53 @@
 #!/usr/bin/node
-/**
- * A script that prints all the characters of a Star Wars Movie.
- */
+
 const request = require('request');
+
 const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
 
-if (!movieId) {
-  console.log('Please provide a movie ID');
-  process.exit(1);
-}
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
 
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(url, async (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    try {
-      const fetchCharacterName = (characterUrl) => {
-        return new Promise((resolve, reject) => {
-          request.get(characterUrl, (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-              resolve(JSON.parse(body).name);
-            } else {
-              reject(new Error('Character information request failed!'));
-            }
-          });
-        });
-      };
-      for (const characterUrl of JSON.parse(body).characters) {
-        console.log(await fetchCharacterName(characterUrl));
-      }
-    } catch (err) {
-      console.log('Exception:', err);
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
     }
   } else {
-    console.log('Movie information request failed!');
+    console.error('Error: Got no Characters for some reason');
   }
-});
+};
+
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
